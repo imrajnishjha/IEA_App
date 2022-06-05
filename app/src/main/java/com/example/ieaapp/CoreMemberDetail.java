20790b1030b0c6f862121bc64fec04a131d29d51
Abhishek <abhishekdeupa@gmail.com>
2022-06-05 11:18:37 +0530

    Core Member Contact

--- app/src/main/java/com/example/ieaapp/CoreMemberDetail.java
+++ app/src/main/java/com/example/ieaapp/CoreMemberDetail.java
@@ -1,6 +1,11 @@
 package com.example.ieaapp;
 
+import android.app.Dialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -21,6 +26,7 @@ public class CoreMemberDetail extends AppCompatActivity {
     ImageView coreTeamMemberDetailProfile;
     AppCompatButton coreTeamMemberContactButton, coreTeamMemberDetailBackButton;
     DatabaseReference databaseReference;
+    Dialog coreMemberContactDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +41,9 @@ public class CoreMemberDetail extends AppCompatActivity {
         coreTeamMemberContactButton = findViewById(R.id.core_detail_contact_button);
         coreTeamMemberDetailBackButton = findViewById(R.id.core_member_detail_back_button);
 
+
+        coreMemberContactDialog = new Dialog(this);
+
         String coreItemKey = getIntent().getStringExtra("CoreItemKey");
 
         databaseReference.child(coreItemKey).addValueEventListener(new ValueEventListener() {
@@ -44,6 +53,8 @@ public class CoreMemberDetail extends AppCompatActivity {
                     String coreMemberName = snapshot.child("name").getValue().toString();
                     String coreMemberDesignation = snapshot.child("designation").getValue().toString();
                     String corePictureUrl = snapshot.child("purl").getValue().toString();
+                    String corePhoneNumber = snapshot.child("phone_no").getValue().toString();
+                    String coreEmail = snapshot.child("mail").getValue().toString();
 
                     coreTeamMemberDetailName.setText(coreMemberName);
                     coreTeamMemberDetailDesignation.setText(coreMemberDesignation);
@@ -64,5 +75,39 @@ public class CoreMemberDetail extends AppCompatActivity {
         });
 
         coreTeamMemberDetailBackButton.setOnClickListener(view -> finish());
+
+        coreTeamMemberContactButton.setOnClickListener(view -> {
+
+            databaseReference.child(coreItemKey).addValueEventListener(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot snapshot) {
+                    if(snapshot.exists()){
+
+                        LayoutInflater inflater = getLayoutInflater();
+                        View view = inflater.inflate(R.layout.core_member_contact_popup, null);
+
+                        TextView coreMemberPhoneNumber = view.findViewById(R.id.core_member_phone_number);
+                        TextView coreMemberEmail = view.findViewById(R.id.core_member_email);
+
+                        String coreEmail = snapshot.child("mail").getValue().toString();
+                        String corePhoneNumber = snapshot.child("phone_no").getValue().toString();
+
+                        coreMemberPhoneNumber.setText(corePhoneNumber);
+                        coreMemberEmail.setText(coreEmail);
+
+                        coreMemberContactDialog.setContentView(view);
+                        coreMemberContactDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                        coreMemberContactDialog.show();
+
+                    }
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError error) {
+
+                }
+            });
+
+        });
     }
 }
